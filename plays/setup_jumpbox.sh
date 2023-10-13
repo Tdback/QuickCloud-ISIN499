677a54a677b5ce0c6be0064356fc56b6dd34660a
Tyler <tylerdback.etc@proton.me>
2023-10-12 20:28:57 -0400

    Fixed issues with uploading files and ssh keys to bastion host.

--- plays/setup_jumpbox.sh
+++ plays/setup_jumpbox.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+cd ../ || exit 1
 IP=$(terraform output | awk -F'=' '{ print $2 }' | tr -d ' "')
+
+cd plays/ || exit 1
 
 ansible-playbook ./setup_jumpbox.yaml --extra-vars "variable_host=$IP"
 
