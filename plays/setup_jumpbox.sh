c743623b442dda07678d363927c47007b0a64e1e
Tyler <tylerdback.etc@proton.me>
2023-10-13 13:44:50 -0400

    Created configuration for jumpbox. Need to still add mysql.

--- plays/setup_jumpbox.sh
+++ plays/setup_jumpbox.sh
@@ -5,6 +5,9 @@ IP=$(terraform output | awk -F'=' '{ print $2 }' | tr -d ' "')
 
 cd plays/ || exit 1
 
-ansible-playbook ./setup_jumpbox.yaml --extra-vars "variable_host=$IP"
+if ! ansible-playbook ./run.yaml --extra-vars "variable_host=$IP ssh_port=22"
+then
+    ansible-playbook ./run.yaml --extra-vars "variable_host=$IP"
+fi
 
 # vim: ft=sh
