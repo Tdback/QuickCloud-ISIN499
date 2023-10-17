002e7ca5ad5c882fd588122ffc2e6c530e03a480
0Ragnor <pearisoalex@gmail.com>
2023-10-17 14:58:46 -0400

    changed database username and password

--- bastion/site/config.php
+++ bastion/site/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', '10.0.6.100');
-define('DB_USER', 'peara');
-define('DB_PASS', 'password123');
+define('DB_USER', 'admin');
+define('DB_PASS', 'password');
 define('DB_NAME', 'quick_cloud_db');
 ?>
