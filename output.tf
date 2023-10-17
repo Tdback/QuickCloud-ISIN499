e3e7d5a6bfb90e9ddb7cb2bed54ba039f2716ee9
Tyler <tylerdback.etc@proton.me>
2023-10-17 14:53:36 -0400

    Added dns name to output.

--- output.tf
+++ output.tf
@@ -1,5 +1,11 @@
 output "instance_ip" {
-  value = aws_instance.jump_box.public_ip
+  value       = aws_instance.jump_box.public_ip
+  description = "IP address of public-facing jumpbox."
+}
+
+output "alb_domain_name" {
+  value       = aws_lb.quickcloud_alb.dns_name
+  description = "Domain name of the load balancer."
 }
 
 # vim: ft=terraform : ts=2
