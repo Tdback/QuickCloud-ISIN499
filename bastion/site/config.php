e38e77828c46947d3c7b161246d8c7a915aff8f2
0Ragnor <pearisoalex@gmail.com>
2023-10-17 12:41:56 -0400

    removed old index file with an updated one, removed html line from vars

--- bastion/site/config.php
+++ bastion/site/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', '10.0.6.100');
 define('DB_USER', 'peara');
-define('DB_PASS', 'your_password');
+define('DB_PASS', 'password123');
 define('DB_NAME', 'quick_cloud_db');
 ?>
